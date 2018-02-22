cbef028a9b8893f7728ef347bbf0b002720e6280
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-02-22 23:30:08 +0100

    Fully working project.

--- josephus_problem.rb
+++ josephus_problem.rb
@@ -1,5 +1,3 @@
-
-
 class Ddly_Crl
 	class Person
 		@@id = 0
@@ -36,12 +34,30 @@ class Ddly_Crl
 		end
 	end
 
-	def kll_pss(person) #this method will kill object neighbor and pass "sword" to the next one
-		if person.sword?
+	def kll_pss(person) #this method will kill neighbor object and pass "sword" to the next one
+		if person.sword == true
 			tmp_nbr = person.nbr.nbr #it assign the "victims" neighbor
+			puts "Person #{person.nbr.num} died!"
+			@crl.delete_at(@crl.index(person.nbr))
+			person.nbr = tmp_nbr
+			person.nbr.sword = true
+			person.sword = false
+		else
+			puts "No sword!" #just in case if sth go wrong, but this part of code should never execute
 		end	
-	end	
+	end
+	
+	@public
+	def nightmare_begin #this method is looping through our "deadly circle"
+		@crl[0].sword = true # first obj must get sword somehow 
+		while @crl.size != 1
+			@crl.each do |person|
+				puts "Now person: #{person.num}"
+				kll_pss(person)
+			end
+		end
+		puts "Person #{@crl[0].num} survived!"
+	end
 end
 
-x = Ddly_Crl.new(8)
-puts x.crl
+joseph = Ddly_Crl.new(gets.chomp.to_i).nightmare_begin

1aae9d0378ca96477a45ae622bf9ddfd5d542965
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-02-22 19:38:16 +0100

    Add working ids assinment

--- josephus_problem.rb
+++ josephus_problem.rb
@@ -9,16 +9,30 @@ class Person
 		@nbr = nil 
 		@num = @@id
 	end
+	
+	def to_s
+		"Person id is #{self.object_id} and it's neigbour is #{@nbr}"
+	end
 end
 
 class Ddly_Crl
 	attr_accessor:crl
 	def initialize(size)
 		@crl = Array.new(size) {Person.new}
+		nbr_asi
 	end
 	
-	def nbr_asi
+	@private
+	def nbr_asi #this method will assign id of an next adjacent object 
 		@crl.each do |person|
-			person.nbr = @crowd.index(person)
+			if @crl[-1] == person
+				person.nbr = @crl[0].object_id #if object is the last element or an array it assign the id of the first element to make a circle
+			else
+				person.nbr = @crl[(@crl.index(person)+1)].object_id #it assign next element id to the current obcject
+			end
+		end
 	end
 end
+
+x = Ddly_Crl.new(1)
+puts x.crl
